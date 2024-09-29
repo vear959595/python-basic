fc91210065ba0b9e051d048c91d3d65129cc65f2
Artem Veber <veberav1201@yandex.ru>
2024-09-29 14:31:26 +0700

    Add comments to 8.5.py

--- 8.5.py
+++ 8.5.py
@@ -1,9 +1,10 @@
+#Exercise 1
 while True:
     value = input("Type q or Q to exit: ")
     if value.lower() == "q":
         break
 
-
+#Exercise 2
 for i in range(1, 51):
     if i % 3 == 0:
         continue
