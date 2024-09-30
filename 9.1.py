c714d5d3223e00204593aa34ae62c55d17451ce1
Artem Veber <veberav1201@yandex.ru>
2024-09-30 16:27:18 +0700

    add 9.1.py

---
+++ 9.1.py
@@ -0,0 +1,27 @@
+#Exercise 1
+
+cardinal_number = ("first", "second", "third")
+
+#Exercise2
+
+print(cardinal_number[1])
+
+#Exercise 3
+
+position1, position2, position3 = cardinal_number
+
+print(position1)
+print(position2)
+print(position3)
+
+#Exercise 4
+
+my_name = tuple("Artem")
+
+#Exercise 5
+
+print("x" in my_name)
+
+#Exercise 6
+
+print(my_name[1:])
