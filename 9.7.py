0f4a54499f5421b74c2adec52c686eb0a79c90a7
Artem Veber <veberav1201@yandex.ru>
2024-10-09 00:31:49 +0700

    add print of state

--- 9.7.py
+++ 9.7.py
@@ -5,6 +5,7 @@ from capitals import capitals_dict
 state, capital = random.choice(list(capitals_dict.items()))
 
 while True:
+    print(f"What is the capital of {state}")
     answer = input("Enter your answer or type \"Exit\": ")
     if answer.lower() == capital.lower():
         print("Correct")
