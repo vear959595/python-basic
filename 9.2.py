46c8f11233e5297f6f0c9237354a1bcf33129372
Artem Veber <veberav1201@yandex.ru>
2024-10-08 19:13:51 +0700

    add 9.2.py

---
+++ 9.2.py
@@ -0,0 +1,27 @@
+#Exercise 1
+
+food = ["rice", "beans"]
+
+#Exercise 2
+
+food.append("broccoli")
+
+#Exercise 3
+
+food.extend(["bread", "pizza"])
+
+#Exercise 4
+
+print(food[:2])
+
+#Exercise 5
+
+print(food[-1])
+
+#Exercise 6
+text = "eggs, fruit, orangejuice"
+breakfast = text.split(",")
+
+#Exercise 7
+
+print(len(breakfast))
