bce4279048bfa65b20565c3bad25c2280110d4f5
Artem Veber <veberav1201@yandex.ru>
2024-10-09 19:28:12 +0700

    add 10.3.py

---
+++ 10.3.py
@@ -0,0 +1,39 @@
+#Exercise 1
+class Dog:
+    species = "Canis familiars"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+class GoldenRetriever(Dog):
+    def speak(self, sound="Bark"):
+        return super().speak(sound)
+
+#Exercise 2
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+class Square(Rectangle):
+    def __init__(self, side_length):
+        super().__init__(side_length, side_length)
+
+test = Square(4)
+
+print(test.area())
+
+test.width = 5
+print(test.area())
